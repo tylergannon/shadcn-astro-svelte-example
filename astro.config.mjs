a14467416c51c20fefd07f135cb7f3f4ed3c1387
Tyler Gannon <git@tylergannon.com>
2024-02-06 20:30:41 -0800

    shadcn prerequisites

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
+import path from 'path';
 import svelte from '@astrojs/svelte';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), tailwind({ applyBaseStyles: false })]
+	integrations: [svelte(), tailwind({ applyBaseStyles: false })],
+	vite: {
+		resolve: { alias: { $lib: path.resolve('./src/lib') } },
+		ssr: { noExternal: ['lucide-svelte'] }
+	}
 });
